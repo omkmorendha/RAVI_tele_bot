3923d38ded3e8547218bfc7889eca2fd74c0cc73
Om Morendha <omkmorendha@gmail.com>
2024-01-13 23:53:37 +0530

    add zip file capability to downloads

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ from dotenv import load_dotenv
 import yaml
 import boto3
 import re
+import base64
+from io import BytesIO, StringIO
+import zipfile
 from yaml.loader import SafeLoader
 
-with open('./config.yaml') as file:
+with open("./config.yaml") as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 load_dotenv()
@@ -24,132 +27,144 @@ RDS_DATABASE = os.environ.get("RDS_DATABASE")
 S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
 
 # Create a connection to the RDS database
-engine = create_engine(f"mysql+mysqlconnector://{RDS_USER}:{RDS_PASSWORD}@{RDS_HOST}:{RDS_PORT}/{RDS_DATABASE}")
+engine = create_engine(
+    f"mysql+mysqlconnector://{RDS_USER}:{RDS_PASSWORD}@{RDS_HOST}:{RDS_PORT}/{RDS_DATABASE}"
+)
+
 
 def save():
-    with open('config.yaml', 'w') as file:
+    with open("config.yaml", "w") as file:
         yaml.dump(config, file, default_flow_style=False)
-        
-def download_s3_object(url):
-    # Extract the bucket name and object key from the S3 URL
-    s3_parts = url.split("/")
-    bucket_name_extra = s3_parts[2]
-    bucket_name = re.match(r'^([^\.]+)\.', bucket_name_extra).group(1)
-    object_key = "/".join(s3_parts[3:])
 
+
+def download_s3_objects_as_zip(urls, zip_filename):
     try:
-        # Create a Boto3 S3 client
         s3_client = boto3.client(
             "s3",
-            region_name='eu-north-1',
+            region_name="eu-north-1",
             aws_access_key_id=AWS_ACCESS_KEY_ID,
             aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-            config=boto3.session.Config(signature_version='s3v4')
+            config=boto3.session.Config(signature_version="s3v4"),
         )
 
-        # Get a pre-signed URL for the S3 object
-        presigned_url = s3_client.generate_presigned_url(
-            'get_object',
-            Params={'Bucket': bucket_name, 'Key': object_key},
-            ExpiresIn=3600  # URL expiration time in seconds (adjust as needed)
-        )
+        zip_buffer = BytesIO()
+        with zipfile.ZipFile(zip_buffer, "a", zipfile.ZIP_DEFLATED) as zip_file:
+            for url in urls:
+                s3_parts = url.split("/")
+                bucket_name_extra = s3_parts[2]
+                bucket_name = re.match(r"^([^\.]+)\.", bucket_name_extra).group(1)
+                object_key = "/".join(s3_parts[3:])
+                file_data = s3_client.get_object(Bucket=bucket_name, Key=object_key)[
+                    "Body"
+                ].read()
+                zip_file.writestr(os.path.basename(object_key), file_data)
 
-        # Create a custom HTML button with JavaScript to trigger the file download
-        download_button = f'<a href="{presigned_url}" download>Download {object_key}</a>'
-        st.markdown(download_button, unsafe_allow_html=True)
+        zip_buffer.seek(0)
+
+        st.markdown(
+            f'<a href="data:application/zip;base64,{base64.b64encode(zip_buffer.getvalue()).decode()}" '
+            f'download="{zip_filename}.zip">Download All Files as Zip</a>',
+            unsafe_allow_html=True,
+        )
 
     except Exception as e:
         st.error(f"An error occurred while generating the download link: {e}")
 
-        
+
 def table_ind_page():
     st.title("RAVI BOT SUBMISSIONS")
 
-    query = "SELECT * FROM user_data" 
+    query = "SELECT * FROM user_data"
     df = pd.read_sql(query, engine)
 
-    # Display the table using Streamlit with a button for each row
     for index, row in df.iterrows():
-        # Button to open sidebar for the current row
         if st.button(f"Download Files for Individual {index + 1}"):
-            # Sidebar content for the current row
             st.sidebar.title(f"Individual {index + 1} Details")
-
-            # Display other columns if needed
             st.sidebar.write("Data:")
-            st.sidebar.write(row.drop(["Final_Testimony_URL", "Additional_Evidence_URL"]))
+            st.sidebar.write(
+                row.drop(["Final_Testimony_URL", "Additional_Evidence_URL"])
+            )
 
-            # Display Final Testimony Files
-            st.sidebar.write("\nFinal Testimony Files:")
             final_testimony_urls = row["Final_Testimony_URL"]
             if final_testimony_urls:
                 final_testimony_urls = final_testimony_urls.split()
+                st.sidebar.write("\nFinal Testimony Files:")
                 for url in final_testimony_urls:
-                    download_s3_object(url)
                     st.sidebar.write(f"- [View {os.path.basename(url)}]({url})")
-            else:
-                st.sidebar.write("No Files available")
 
-            # Display Additional Evidence Files
-            st.sidebar.write("\nAdditional Evidence Files:")
             additional_evidence_urls = row["Additional_Evidence_URL"]
             if additional_evidence_urls:
                 additional_evidence_urls = additional_evidence_urls.split()
+                st.sidebar.write("\nAdditional Evidence Files:")
                 for url in additional_evidence_urls:
-                    download_s3_object(url)
                     st.sidebar.write(f"- [View {os.path.basename(url)}]({url})")
+
+            if additional_evidence_urls or final_testimony_urls:
+                urls = []
+                if isinstance(final_testimony_urls, list) and isinstance(
+                    additional_evidence_urls, list
+                ):
+                    urls = additional_evidence_urls + final_testimony_urls
+                elif isinstance(final_testimony_urls, list):
+                    urls = final_testimony_urls
+                elif isinstance(additional_evidence_urls, list):
+                    urls = additional_evidence_urls
+
+                download_s3_objects_as_zip(urls, f"Individual_{index + 1}_Files.zip")
             else:
-                st.sidebar.write("No Files available")
+                st.write(f"No Files to download for Individual_{index + 1}")
 
 
 def table_full_page():
     st.title("RAVI BOT SUBMISSIONS")
 
-    query = "SELECT * FROM user_data" 
+    query = "SELECT * FROM user_data"
     df = pd.read_sql(query, engine)
 
     # Display the table using Streamlita
     df.index += 1
     st.dataframe(df, width=0, height=0)
 
+
 def main():
     st.set_page_config(layout="wide")
 
     authenticator = stauth.Authenticate(
-        config['credentials'],
-        config['cookie']['name'],
-        config['cookie']['key'],
-        config['cookie']['expiry_days'],
+        config["credentials"],
+        config["cookie"]["name"],
+        config["cookie"]["key"],
+        config["cookie"]["expiry_days"],
     )
-    name, authentication_status, username = authenticator.login('Login', 'main')
-    
+    name, authentication_status, username = authenticator.login("Login", "main")
+
     if st.session_state["authentication_status"]:
         data = st.selectbox(
             "Option",
             (
                 "See Full Database",
                 "View and Download Individual Testimonies and Evidence",
-                "Reset Password"
+                "Reset Password",
             ),
         )
-        
+
         if data == "View and Download Individual Testimonies and Evidence":
             table_ind_page()
-        
+
         if data == "See Full Database":
             table_full_page()
-        
+
         elif data == "Reset Password":
-            if authenticator.reset_password(username, 'Reset password'):
-                st.success('Password modified successfully')
+            if authenticator.reset_password(username, "Reset password"):
+                st.success("Password modified successfully")
                 save()
-            
-        authenticator.logout('Logout', 'main')
-        
+
+        authenticator.logout("Logout", "main")
+
     elif st.session_state["authentication_status"] == False:
-        st.error('Username/password is incorrect')
+        st.error("Username/password is incorrect")
     elif st.session_state["authentication_status"] == None:
-        st.warning('Please enter your username and password')
-        
+        st.warning("Please enter your username and password")
+
+
 if __name__ == "__main__":
     main()
